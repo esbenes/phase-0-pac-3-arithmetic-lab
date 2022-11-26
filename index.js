23583af8426090350cda0c24e01a60044f9ef880
Enes <enessalihbcelebi@gmail.com>
2022-11-26 22:47:22 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(){
+    return 3+8;
+}
+function subtract(){
+    return 21-10;
+}
+function multiply(){
+    11*1;
+}
+function divide(){
+    return 11/1;
+}
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function divide(a, b){
+    return a/b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function increment(a){
+    return a +=1;
+}
+function decrement(a){
+    return a -=1;
+}
+function makeInt(a){
+    return parseInt(a, 10);
+}
+function preserveDecimal(a){
+    return parseFloat(a);
+}
